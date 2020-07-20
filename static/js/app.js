34c57b4b722a8af2ac1f339cbf8b097f6cd6e822
rbourdeau1 <63135478+rbourdeau1@users.noreply.github.com>
2020-07-19 23:24:53 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,21 @@ d3.json('samples.json').then((data) => {
     var samples = data.samples;
     var chosenSample = samples.filter(row => row.id == '940');
     var result = chosenSample[0];
-    console.log(result)
+    console.log(result);
+    
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+
+    var trace = {
+        y: otu_ids,
+        x: sample_values,
+        text: otu_labels,
+        type: 'bar',
+        orientation: 'h'
+    };
+
+    var data = [trace];
+
+    Plotly.newPlot('bar', data); 
 })
