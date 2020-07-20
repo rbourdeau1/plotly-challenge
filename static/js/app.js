4f2f5b0be3ec3087ef1de6fc2a3669ad49554acb
rbourdeau1 <63135478+rbourdeau1@users.noreply.github.com>
2020-07-19 23:05:30 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,5 @@ d3.json('samples.json').then((data) => {
     var samples = data.samples;
     var chosenSample = samples.filter(row => row.id == '940');
     var result = chosenSample[0];
-    console.log('result')
+    console.log(result)
 })
